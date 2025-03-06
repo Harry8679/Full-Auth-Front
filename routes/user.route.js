c3cb8a8f399ff89cc7ed993fb2d1ee3283e280d0
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-06 23:58:06 +0100

    Verify Email route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 // routes/user.route.js
 const express = require('express');
-const { register, login } = require('../controllers/user.controller');
+const { register, login, verifyEmail } = require('../controllers/user.controller');
 const router = express.Router();
 
 // Registration
 router.post('/register', register);
 router.post('/login', login);
+router.get('/verify/:token', verifyEmail);
 
 module.exports = router; // Exporte le routeur
