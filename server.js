1c267118430d3fde112ba2b215b5cd6d557df8a7
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-06 17:04:29 +0100

    Server 2

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+dotenv.config();
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Connexion à MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server launched on the port ${PORT}`));
