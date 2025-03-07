66ea437960f432abd0ac8ed1d42b13bd2fef9750
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-07 13:10:57 +0100

    Auth Middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,14 @@
+const authMiddleware = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ error: 'Accès refusé, token manquant' });
+ 
+  try {
+    const verified = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).json({ error: 'Token invalide' });
+  }
+};
+
+export default authMiddleware;
