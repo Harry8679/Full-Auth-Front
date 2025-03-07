b9e17271575ac6e709e06e6a25da0f9b31a643b5
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-07 10:41:55 +0100

    Login Controller updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -48,7 +48,7 @@ const login = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token, user });
   } catch(err) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: err.message });
   }
 }
 
