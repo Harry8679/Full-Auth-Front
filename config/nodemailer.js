7b2b73077baf014ff1d2031292d78ff84af997ca
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-07 15:53:06 +0100

    Nodemailer 5

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,9 +2,9 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp-mail.outlook.com', // Pour les comptes Outlook
-  port: 587, // Utilisation du port SMTP standard
-  secure: false, // Doit être false pour STARTTLS
+  host: 'smtp-mail.outlook.com', // Serveur SMTP Outlook
+  port: 587, // Port standard pour Outlook SMTP
+  secure: false, // STARTTLS doit être activé
   auth: {
     user: process.env.EMAIL_USER, // Ton email Outlook
     pass: process.env.EMAIL_PASS, // Ton mot de passe d'application
