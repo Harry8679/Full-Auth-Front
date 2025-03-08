6b7d971b354c4e30bd85352fca3b72bfad1653c3
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-08 07:01:28 +0100

    Nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,18 +1,22 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp-mail.outlook.com',
-  port: 587,
-  secure: false, // STARTTLS doit être activé
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  requireTLS: true, // Forcer l'utilisation de STARTTLS
-  tls: {
-    rejectUnauthorized: false, // Désactiver la vérification du certificat
-  },
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-module.exports = transporter;
+const sendEmail = async (to, subject, text) => {
+  const msg = {
+    to,
+    from: 'ton-email@ton-domaine.com', // L'email vérifié dans SendGrid
+    subject,
+    text,
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log('✅ Email envoyé avec succès !');
+  } catch (error) {
+    console.error('❌ Erreur envoi email:', error);
+  }
+};
+
+module.exports = sendEmail;
