7576e008eafb5d05f5dae5d3e5153ca80e2461e1
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-08 07:00:41 +0100

    Sendgrid installed

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,15 +2,16 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp-mail.outlook.com', // Serveur SMTP Outlook
-  port: 587, // Port standard pour Outlook SMTP
+  host: 'smtp-mail.outlook.com',
+  port: 587,
   secure: false, // STARTTLS doit être activé
   auth: {
-    user: process.env.EMAIL_USER, // Ton email Outlook
-    pass: process.env.EMAIL_PASS, // Ton mot de passe d'application
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
+  requireTLS: true, // Forcer l'utilisation de STARTTLS
   tls: {
-    rejectUnauthorized: false, // Évite certains problèmes SSL
+    rejectUnauthorized: false, // Désactiver la vérification du certificat
   },
 });
 
