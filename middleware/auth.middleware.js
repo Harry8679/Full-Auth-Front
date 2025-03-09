e88a46123dff86fd9a671cfc78165b904379ef39
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-09 17:13:03 +0100

    Auth Middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,13 +1,20 @@
+const jwt = require("jsonwebtoken");
+
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ error: 'Accès refusé, token manquant' });
- 
+  const authHeader = req.headers.authorization;
+  
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Accès refusé, token manquant !" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
   try {
-    const verified = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
-    req.user = verified;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // ✅ Stocke les données du token dans `req.user`
     next();
   } catch (error) {
-    res.status(400).json({ error: 'Token invalide' });
+    return res.status(401).json({ error: "Token invalide" });
   }
 };
 
