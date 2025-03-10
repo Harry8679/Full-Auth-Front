42907bbc68e1a855065fab9e8f8fd6f66db46f35
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-10 07:26:53 +0100

    Change Password Route

--- routes/user.route.js
+++ routes/user.route.js
@@ -28,5 +28,6 @@ router.put('/profile', authMiddleware, updateProfile);
 // 📌 ✅ Routes pour la récupération de mot de passe
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
+router.post('/change-password/', resetPassword);
 
 module.exports = router;
